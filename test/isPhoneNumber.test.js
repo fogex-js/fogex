0e7fe89092bbe5b77f1a8fac0668df09273ce30f
ymzEmre <dev.yilmazemre@gmail.com>
2022-05-01 07:20:40 +0300

    Edit test value in isPhoneNumber (#3)

--- test/isPhoneNumber.test.js
+++ test/isPhoneNumber.test.js
@@ -1,10 +1,8 @@
 import isPhoneNumber from '../fogex/regex/isPhoneNumber';
 
 it('should return true for phone format', () => {
-  expect(isPhoneNumber('+905357894520', 'TR')).toBe(true);
-  expect(isPhoneNumber('905357894520', 'TR')).toBe(true);
-  expect(isPhoneNumber('05357894520', 'TR')).toBe(true);
-  expect(isPhoneNumber('5357894520', 'TR')).toBe(true);
-  expect(isPhoneNumber('+1 800 5551212', 'US')).toBe(true);
-  expect(isPhoneNumber('1 800 5551212', 'US')).toBe(true);
+  expect(isPhoneNumber('+905555555555', 'TR')).toBe(true);
+  expect(isPhoneNumber('905555555555', 'TR')).toBe(true);
+  expect(isPhoneNumber('5555555555', 'TR')).toBe(true);
+  expect(isPhoneNumber('+1 123 4567890', 'US')).toBe(true);
 });
