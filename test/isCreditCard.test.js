911d2021875a13939c31264d7a84852de69dbb0e
ymzEmre <dev.yilmazemre@gmail.com>
2022-05-01 07:20:04 +0300

    Edit test value in isCreditCard (#1)

--- test/isCreditCard.test.js
+++ test/isCreditCard.test.js
@@ -3,7 +3,6 @@ import isCreditCard from '../fogex/regex/isCreditCard';
 it('should return true for valid credit card values', () => {
   expect(isCreditCard('1111222233334444')).toBe(true);
   expect(isCreditCard('1111 2222 3333 4444')).toBe(true);
-  expect(isCreditCard('4140554247281917', 'VISA')).toBe(true);
   expect(isCreditCard('4140 5542 4728 1917', 'VISA')).toBe(true);
   expect(isCreditCard('4002293472493304', 'VISA')).toBe(true);
   expect(isCreditCard('4002 2934 7249 3304', 'VISA')).toBe(true);
