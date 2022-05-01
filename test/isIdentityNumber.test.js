ad448879d7bff738fc662473f464d9c98077fbdd
ahmet021 <ahmetozkan021@gmail.com>
2022-05-01 13:27:28 +0300

    Add new tests to function isIdentityNumber (#5)

--- test/isIdentityNumber.test.js
+++ test/isIdentityNumber.test.js
@@ -1,5 +1,9 @@
-import isIdentityNumber from '../fogex/regex/isIdentityNumber';
+import isIdentityNumber from "../fogex/regex/isIdentityNumber";
 
-it('should return true for identity number format', () => {
-  expect(isIdentityNumber('12345678910', 'TR')).toBe(true);
+it("should return true for identity number format", () => {
+  expect(isIdentityNumber("11111111110", "TR")).toBe(true);
+  expect(isIdentityNumber("22222222222", "TR")).toBe(true);
+  expect(isIdentityNumber("011111111111", "TR")).toBe(false);
+  expect(isIdentityNumber("12222222221", "TR")).toBe(false);
+  expect(isIdentityNumber("001-01-0011", "US")).toBe(true);
 });
