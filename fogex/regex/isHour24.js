bc0febaa5188f9955c99a923375bae84c20022d5
ahmet021 <ahmetozkan021@gmail.com>
2022-05-03 15:39:06 +0300

    Fix the hours in parametric (#8)
    Delete test legacy

--- fogex/regex/isHour24.js
+++
@@ -1,8 +0,0 @@
-let patternHour24 = new RegExp(/^([0-1][0-9]|2[0-3]):([0-5][0-9])$/);
-
-const isHour24 = (value) => {
-  if (value == null || patternHour24 == '/(?:)/') return;
-  return patternHour24.test(value);
-};
-
-export default isHour24;
--- test/isHour12.test.js
+++
@@ -1,7 +0,0 @@
-import isHour12 from '../fogex/regex/isHour12';
-
-it('should return true for am-pm format', () => {
-  expect(isHour12('12:00am')).toBe(true);
-  expect(isHour12('01:00pm')).toBe(true);
-  expect(isHour12('10:15am')).toBe(true);
-});
