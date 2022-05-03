53ea52c6cea2f884db9464e0946342038ccc5975
ahmet021 <ahmetozkan021@gmail.com>
2022-05-03 15:37:36 +0300

    Fix the text case in parametric (#7)
    Delete test legacy

--- test/isCamelCase.test.js
+++
@@ -1,5 +0,0 @@
-import isCamelCase from '../fogex/regex/isCamelCase';
-
-it('should return true for valid camelCase strings', () => {
-  expect(isCamelCase('testTest')).toBe(true);
-});
--- test/isLowerCase.test.js
+++
@@ -1,5 +0,0 @@
-import isLowerCase from '../fogex/regex/isLowerCase';
-
-it('should return true for valid lowercase values', () => {
-  expect(isLowerCase('abc')).toBe(true);
-});
