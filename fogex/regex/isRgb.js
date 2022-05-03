735c070891a39ed4a2a82b356243cbab66acc606
ahmet021 <ahmetozkan021@gmail.com>
2022-05-03 15:36:09 +0300

    Fix the color codes in parametric (#6)
    Delete legacy test

--- fogex/regex/isRgb.js
+++
@@ -1,8 +0,0 @@
-const patternRgb = new RegExp(/^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/);
-
-const isRgb = (value) => {
-  if (value == null || patternRgb == '/(?:)/') return;
-  return patternRgb.test(value);
-};
-
-export default isRgb;
