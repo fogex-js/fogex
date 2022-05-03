ba9f535532a644c40d9eba3c4d606818589cbf0f
ahmet021 <ahmetozkan021@gmail.com>
2022-05-03 15:39:06 +0300

    Fix the hours in parametric (#8)
    Delete test legacy

--- test/isHour24.test.js
+++
@@ -1,7 +0,0 @@
-import isHour24 from '../fogex/regex/isHour24';
-
-it('should return true for valid hour values', () => {
-  expect(isHour24('12:00')).toBe(true);
-  expect(isHour24('01:00')).toBe(true);
-  expect(isHour24('10:15')).toBe(true);
-});
