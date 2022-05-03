09f172125ef81ea35914a8dc920275def7478d49
ahmet021 <ahmetozkan021@gmail.com>
2022-05-03 15:36:09 +0300

    Fix the color codes in parametric (#6)
    Delete legacy test

--- test/isHex.test.js
+++
@@ -1,6 +0,0 @@
-import isHex from '../fogex/regex/isHex';
-
-it('should return true for valid hash values', () => {
-  expect(isHex('#addaca')).toBe(true);
-  expect(isHex('addaca')).toBe(true);
-});
--- test/isHsl.test.js
+++
@@ -1,5 +0,0 @@
-import isHsl from '../fogex/regex/isHsl';
-
-it('should return true for valid hsl values', () => {
-  expect(isHsl('hsl(0, 100%, 50%)')).toBe(true);
-});
