a098028f0bbd87d5851d85a26f7169ec0bf002ba
ahmet021 <ahmetozkan021@gmail.com>
2022-05-03 15:36:09 +0300

    Fix the color codes in parametric (#6)
    Delete legacy test

--- fogex/regex/isHex.js
+++
@@ -1,8 +0,0 @@
-const patternHex = new RegExp(/^#?([a-f0-9]{6}|[a-f0-9]{3})$/i);
-
-const isHex = (value) => {
-  if (value == null || patternHex == '/(?:)/') return;
-  return patternHex.test(value);
-};
-
-export default isHex;
