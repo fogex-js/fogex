637b1cae68efdae5669c7977aa93de9492585ce1
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-01-23 22:20:49 +0300

    Add new tests

--- test/isBoolean.test.ts
+++ test/isBoolean.test.ts
@@ -3,4 +3,5 @@ import isBoolean from '../src/patterns/isBoolean';
 it('should return true for valid boolean values', () => {
   expect(isBoolean(true)).toBe(true);
   expect(isBoolean(false)).toBe(true);
+  expect(isBoolean('false')).toBe(false);
 });
