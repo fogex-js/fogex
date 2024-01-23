af597a4fab5eb76e9fcb06e3fe05d3907b6d87bb
ymzemre <dev.yilmazemre@gmail.com>
2024-01-23 21:18:51 +0300

    Fix folder structure

--- test/isIsbn.test.ts
+++ test/isIsbn.test.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import isISBN from '../src/patterns/isIsbn';
-=======
-import isISBN from '../fogex/patterns/isIsbn';
->>>>>>> 6177931 (Add index.d.ts and fix jest for typescript)
 
 it('should return true for valid isbn strings', () => {
   expect(isISBN('978-1-56619-909-4')).toBe(true);
