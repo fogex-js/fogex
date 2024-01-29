a57d40b2988d7f3bcac809918471d464c7284c38
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-01-29 13:32:17 +0300

    Add new tes

--- test/isPhoneNumber.test.ts
+++ test/isPhoneNumber.test.ts
@@ -5,4 +5,6 @@ it('should return true for phone format', () => {
   expect(isPhoneNumber('905555555555', 'TR')).toBe(true);
   expect(isPhoneNumber('5555555555', 'TR')).toBe(true);
   expect(isPhoneNumber('+1 123 4567890', 'US')).toBe(true);
+  expect(isPhoneNumber('+11234567890', 'US')).toBe(true);
+  expect(isPhoneNumber('1234567890', 'US')).toBe(true);
 });
