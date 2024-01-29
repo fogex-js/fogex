4e01d5ed59dd1386e89be0d3ab2a4870f42ec44d
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-01-29 13:37:20 +0300

    Add new test for isISBN function

--- test/isIsbn.test.ts
+++ test/isIsbn.test.ts
@@ -3,4 +3,5 @@ import isISBN from '../src/patterns/isIsbn';
 it('should return true for valid isbn strings', () => {
   expect(isISBN('978-1-56619-909-4')).toBe(true);
   expect(isISBN('1-56619-909-3')).toBe(true);
+  expect(isISBN('978 1 56619 909 4')).toBe(true);
 });
