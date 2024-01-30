8cdb4ef777e39aa79c23ab8a7edee3edcdbad557
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-01-30 10:17:17 +0300

    Add new test

--- src/patterns/isAscii.ts
+++ src/patterns/isAscii.ts
@@ -1,8 +1,8 @@
 const pattern = new RegExp(/^[\x00-\x7F]*$/)
 
-const isAscii = (value:string) => {
+const isAscii = (value:string | number) => {
   if (value === null || pattern === new RegExp('/(?:)/')) return
-  return pattern.test(value)
+  return pattern.test(value.toString())
 }
 
 export default isAscii
