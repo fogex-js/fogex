b204527b573ff480e6d5d74171c912db05a56f6d
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-02-02 16:23:59 +0300

    Replace class name `GeneratorPattern` instead of `CreatorPattern`

---
+++ src/generatorPattern/GeneratorPattern.ts
@@ -0,0 +1,60 @@
+interface Options {
+  minLength?: number
+  maxLength?: number
+  lowerCase?: boolean
+  upperCase?: boolean
+  number?: boolean
+  specialCharacter?: boolean
+}
+
+class GeneratorPattern {
+  private regexPattern: string
+
+  constructor(options: Options) {
+    this.regexPattern = this.buildRegexPattern(options)
+  }
+
+  private buildRegexPattern(options: Options): string {
+    const optionChecks: { [K in keyof Options]: () => string } = {
+      lowerCase: () =>
+        options.lowerCase ? this.includeLowerCase() : '(?!.*[a-z])',
+      upperCase: () =>
+        options.upperCase ? this.includeUpperCase() : '(?!.*[A-Z])',
+      number: () => (options.number ? this.includeNumber() : '(?!.*\\d)'),
+      specialCharacter: () =>
+        options.specialCharacter
+          ? this.includeSpecialCharacter()
+          : '(?!.*[!@#$%^&*()_+\\-=\\[\\]{};:\'",.<>/?])',
+    }
+
+    const patterns = Object.values(optionChecks).map((generatePattern) =>
+      generatePattern()
+    )
+    return `^${patterns.join('')}.{${options.minLength || 0},${
+      options.maxLength || ''
+    }}$`
+  }
+
+  private includeLowerCase(): string {
+    return '(?=.*[a-z])'
+  }
+
+  private includeUpperCase(): string {
+    return '(?=.*[A-Z])'
+  }
+
+  private includeNumber(): string {
+    return '(?=.*\\d)'
+  }
+
+  private includeSpecialCharacter(): string {
+    return '(?=.*[!@#$%^&*()_+\\-=\\[\\]{};:\'",.<>/?])'
+  }
+
+  validate(value: string): boolean {
+    const regex = new RegExp(this.regexPattern)
+    return regex.test(value)
+  }
+}
+
+export default GeneratorPattern
--- test/generatorPattern/GeneratorPattern.test.ts
+++ test/generatorPattern/GeneratorPattern.test.ts
@@ -1,318 +1,320 @@
 import { expect, it } from '@jest/globals'
-import CreatorPattern from '../../src/creatorPattern/CreatorPattern'
+import GeneratorPattern from '../../src/generatorPattern/GeneratorPattern'
 
 it('should return true for minLength 3', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     minLength: 3,
+    lowerCase: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(true)
+  expect(generatorPattern.validate('aaa')).toBe(true)
 })
 
 it('should return false for minLength 3', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     minLength: 3,
   })
-  expect(creatorPattern.validate('ab')).toBe(false)
+  expect(generatorPattern.validate('ab')).toBe(false)
 })
 
 it('should return true for maxLength 3', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     maxLength: 3,
   })
-  expect(creatorPattern.validate('abc')).toBe(true)
+  expect(generatorPattern.validate('abc')).toBe(true)
 })
 
 it('should return false for maxLength 3', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     maxLength: 3,
   })
-  expect(creatorPattern.validate('abcd')).toBe(false)
+  expect(generatorPattern.validate('abcd')).toBe(false)
 })
 
 it('should return true for minLength 3 and maxLength 5', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     minLength: 3,
     maxLength: 5,
+    upperCase: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(true)
+  expect(generatorPattern.validate('AAAAA')).toBe(true)
 })
 
 it('should return false for minLength 3 and maxLength 5', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     minLength: 3,
     maxLength: 5,
   })
-  expect(creatorPattern.validate('ab')).toBe(false)
+  expect(generatorPattern.validate('ab')).toBe(false)
 })
 
 it('should return false for minLength 3 and maxLength 5', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     minLength: 3,
     maxLength: 5,
   })
-  expect(creatorPattern.validate('abcdef')).toBe(false)
+  expect(generatorPattern.validate('abcdef')).toBe(false)
 })
 
 it('should return true for lowerCase', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(true)
+  expect(generatorPattern.validate('abc')).toBe(true)
 })
 
 it('should return false for lowerCase', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
   })
-  expect(creatorPattern.validate('ABC')).toBe(false)
+  expect(generatorPattern.validate('ABC')).toBe(false)
 })
 
 it('should return true for upperCase', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
   })
-  expect(creatorPattern.validate('ABC')).toBe(true)
+  expect(generatorPattern.validate('ABC')).toBe(true)
 })
 
 it('should return false for upperCase', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     number: true,
   })
-  expect(creatorPattern.validate('123')).toBe(true)
+  expect(generatorPattern.validate('123')).toBe(true)
 })
 
 it('should return false for number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     number: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('!@#')).toBe(true)
+  expect(generatorPattern.validate('!@#')).toBe(true)
 })
 
 it('should return false for specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for lowerCase and upperCase', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
   })
-  expect(creatorPattern.validate('aBc')).toBe(true)
+  expect(generatorPattern.validate('aBc')).toBe(true)
 })
 
 it('should return false for lowerCase and upperCase', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
   })
-  expect(creatorPattern.validate('123')).toBe(false)
+  expect(generatorPattern.validate('123')).toBe(false)
 })
 
 it('should return true for lowerCase and number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     number: true,
   })
-  expect(creatorPattern.validate('a1b')).toBe(true)
+  expect(generatorPattern.validate('a1b')).toBe(true)
 })
 
 it('should return false for lowerCase and number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     number: true,
   })
-  expect(creatorPattern.validate('ABC')).toBe(false)
+  expect(generatorPattern.validate('ABC')).toBe(false)
 })
 
 it('should return true for lowerCase and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('a!b')).toBe(true)
+  expect(generatorPattern.validate('a!b')).toBe(true)
 })
 
 it('should return false for lowerCase and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('ABC')).toBe(false)
+  expect(generatorPattern.validate('ABC')).toBe(false)
 })
 
 it('should return true for upperCase and number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
     number: true,
   })
-  expect(creatorPattern.validate('A1B')).toBe(true)
+  expect(generatorPattern.validate('A1B')).toBe(true)
 })
 
 it('should return false for upperCase and number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
     number: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for upperCase and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('A!B')).toBe(true)
+  expect(generatorPattern.validate('A!B')).toBe(true)
 })
 
 it('should return false for upperCase and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('1!2')).toBe(true)
+  expect(generatorPattern.validate('1!2')).toBe(true)
 })
 
 it('should return false for number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for lowerCase, upperCase and number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     number: true,
   })
-  expect(creatorPattern.validate('a1B')).toBe(true)
+  expect(generatorPattern.validate('a1B')).toBe(true)
 })
 
 it('should return false for lowerCase, upperCase and number', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     number: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for lowerCase, upperCase and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('a!B')).toBe(true)
+  expect(generatorPattern.validate('a!B')).toBe(true)
 })
 
 it('should return false for lowerCase, upperCase and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for lowerCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('a!1')).toBe(true)
+  expect(generatorPattern.validate('a!1')).toBe(true)
 })
 
 it('should return false for lowerCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for upperCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('A!1')).toBe(true)
+  expect(generatorPattern.validate('A!1')).toBe(true)
 })
 
 it('should return false for upperCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     upperCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for lowerCase, upperCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('a!1B')).toBe(true)
+  expect(generatorPattern.validate('a!1B')).toBe(true)
 })
 
 it('should return false for lowerCase, upperCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
 
 it('should return true for lowerCase, upperCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('a!1B')).toBe(true)
+  expect(generatorPattern.validate('a!1B')).toBe(true)
 })
 
 it('should return false for lowerCase, upperCase, number and specialCharacter', () => {
-  const creatorPattern = new CreatorPattern({
+  const generatorPattern = new GeneratorPattern({
     lowerCase: true,
     upperCase: true,
     number: true,
     specialCharacter: true,
   })
-  expect(creatorPattern.validate('abc')).toBe(false)
+  expect(generatorPattern.validate('abc')).toBe(false)
 })
