011425712cf8335eb66beefbc00a7fff4a3d000c
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-02-02 18:36:37 +0300

    Add use case svelte-kit

---
+++ example/svelte-kit/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { isPin, customPattern, GeneratorPattern } from 'fogex'
+
+  // pre-defined patterns
+  const isPinValue = '1234'
+  const isPinValidate = isPin(isPinValue, 4)
+  console.log('isPinValidate', isPinValidate)
+
+  // custom pattern
+  const customParameterValue = 'qweR123!'
+  const customParameterValidate = customPattern('aA#!', 8, customParameterValue)
+  console.log('customParameterValidate', customParameterValidate)
+
+  // class GeneratorPattern()
+  const generatorPatternValue = 'qweR123!'
+
+  const options = {
+    minLength: 8,
+    maxLength: 16,
+    lowerCase: true,
+    upperCase: true,
+    number: true,
+    specialCharacter: true,
+  }
+
+  const generatorPattern = new GeneratorPattern(options)
+
+  const generatorPatternValidate = generatorPattern.validate(
+    generatorPatternValue,
+  )
+  console.log('generatorPatternValidate', generatorPatternValidate)
+</script>
+
+<h1>Fogex Example</h1>
