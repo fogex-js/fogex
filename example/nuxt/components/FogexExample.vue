bf493ab079061d7e1fe9e1d94af80147656a95cf
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-02-02 18:46:34 +0300

    Fix general examples projects

--- example/nuxt/components/FogexExample.vue
+++ example/nuxt/components/FogexExample.vue
@@ -2,17 +2,21 @@
 import { isPin, customPattern, GeneratorPattern } from 'fogex'
 
 // pre-defined patterns
-const isPinValue = '1234'
-const isPinValidate = isPin(isPinValue, 4)
+const isPinValue: string = '1234'
+const isPinValidate: boolean = isPin(isPinValue, 4) || false
 console.log('isPinValidate', isPinValidate)
 
 // custom pattern
-const customParameterValue = 'qweR123!'
-const customParameterValidate = customPattern('aA#!', 8, customParameterValue)
+const customParameterValue: string = 'qweR123!'
+const customParameterValidate: boolean = customPattern(
+  'aA#!',
+  8,
+  customParameterValue
+)
 console.log('customParameterValidate', customParameterValidate)
 
 // class GeneratorPattern()
-const generatorPatternValue = 'qweR123!'
+const generatorPatternValue: string = 'qweR123!'
 
 const options = {
   minLength: 8,
@@ -23,9 +27,9 @@ const options = {
   specialCharacter: true,
 }
 
-const generatorPattern = new GeneratorPattern(options)
+const generatorPattern: GeneratorPattern = new GeneratorPattern(options)
 
-const generatorPatternValidate = generatorPattern.validate(
+const generatorPatternValidate: boolean = generatorPattern.validate(
   generatorPatternValue
 )
 console.log('generatorPatternValidate', generatorPatternValidate)
