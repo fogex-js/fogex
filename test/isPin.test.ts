86e2919b9f8c71089108092dde9dd6cdb1179f89
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-02-03 10:31:59 +0300

    Add new test

--- test/isPin.test.ts
+++ test/isPin.test.ts
@@ -1,6 +1,12 @@
-import isPin from '../src/patterns/isPin';
+import isPin from '../src/patterns/isPin'
 
 it('should return true for pin format', () => {
-  expect(isPin('1234', 4)).toBe(true);
-  expect(isPin('123456', 6)).toBe(true);
-});
+  expect(isPin('1234', 4)).toBe(true)
+  expect(isPin('12345', 4)).toBe(false)
+  expect(isPin('123', 4)).toBe(false)
+  expect(isPin('1234a', 4)).toBe(false)
+  expect(isPin('123456', 6)).toBe(true)
+  expect(isPin('12345', 6)).toBe(false)
+  expect(isPin('1234567', 6)).toBe(false)
+  expect(isPin('12345s', 6)).toBe(false)
+})
